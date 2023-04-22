9e38b1b88328415b18d1753115ae54a12a5f3b56
pururururum4ik <65440538+Ibgor@users.noreply.github.com>
2023-04-22 15:08:04 -0100

    Create log.py
    log

---
+++ log.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+now = datetime.now().time()     # импорт времечко нынешнее 
+#sab = данное событие
+
+def log (now,sab):   #ФУНКЦИЯ LOG
+  with open (log.txt,r+) with f:
+    f.write(f'main:  time-{now}.  ---> text-{sub} \n')
+    
